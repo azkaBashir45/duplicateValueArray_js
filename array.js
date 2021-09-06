fa93a562584f5d186f512a2d4092350565eb6587
azkaBashir45 <azka.ngxoft@gmail.com>
2021-09-05 23:59:19 -0700

    only one time value in array

--- array.js
+++ array.js
@@ -1,15 +1,38 @@
-var arr=[6,5,8,6,3,3];
-var find;
-for(var i=0;i<arr.length;i++)
-{
-   
-        for(var j=i+1;j<arr.length;j++)
-     
-           if(arr[i]===arr[j]) 
-            console.log(arr[i])
-            
+// input ky ly package install and use ky ly import
+"use strict"
+const ps=require("prompt-sync");
+const prompt=ps();
+var input=prompt('Enter size:')
+var arr=[];
 
-           
+for(var i=0;i<input;i++)
+{
+   arr[i]=prompt('Enter number:')
 }
- 
+ for(var i=0;i<arr.length;i++)
+{
   
+//     console.log('output=  '+arr[i])   
+    var j;
+    for(j=0;j<arr.length;j++)
+    {
+            if(i===j)
+            {
+                    continue
+            }
+            if(arr[i]===arr[j])
+            {
+                    break
+            }
+    }
+    if(j===arr.length)
+    {
+            console.log('\none time out::'+arr[i])
+    }
+    
+  }
+  
+           
+ 
+
+
