25173fcc352441076536ed168c0d0f53afc47460
azkaBashir45 <azka.ngxoft@gmail.com>
2021-09-05 22:34:48 -0700

    duplicate value findout

---
+++ array.js
@@ -0,0 +1,15 @@
+var arr=[6,5,8,6,3,3];
+var find;
+for(var i=0;i<arr.length;i++)
+{
+   
+        for(var j=i+1;j<arr.length;j++)
+     
+           if(arr[i]===arr[j]) 
+            console.log(arr[i])
+            
+
+           
+}
+ 
+  
